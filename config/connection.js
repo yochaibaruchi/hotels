696fcf68556405a81a0f929dfbd80e67177a90c2
yochai baruchi <yochaybaruchi@gmail.com>
2022-11-10 16:58:17 +0200

    update

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const db = mySQL.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    timeout: 80000
+    connectionLimit: 15
 })
 
 
