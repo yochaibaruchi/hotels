07a604874c4de9cb7b82f2a72bd3e8a65516c117
yochai baruchi <yochaybaruchi@gmail.com>
2022-11-10 14:00:47 +0200

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/api/user', userRouter)
 
 
 
-const port = process.env.port || 3003
+const port = process.env.port || 5000
 app.listen(port, () => {
     console.log("app running on port 3003");
 })
