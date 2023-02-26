9c44afea26e7fd1caa45cd9fe1cca594564a6fa9
yochai baruchi <yochaybaruchi@gmail.com>
2023-02-26 13:21:27 +0200

    cluster setup-1

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const cluster = require('cluster');
 const { cpus } = require('os')
-const numCPUs = cpus().length;
+const num_processes = cpus().length;
 const cors = require('cors');
+
+
+if (cluster.isPrimary) {
+    console.log(`primary ${process.pid} is running`)
+
+    let workers = [];
+
+    // Helper function for spawning worker at index 'i'.
+    let spawn = function (i) {
+        workers[i] = cluster.fork();
+
+        // Optional: Restart worker on exit
+        workers[i].on('exit', function (code, signal) {
+            // console.log('respawning worker', i);
+            spawn(i);
+        });
+    };
+
+
+    // Spawn workers.
+    for (var i = 0; i < num_processes; i++) {
+        spawn(i);
+    }
+
+    // Helper function for getting a worker index based on IP address.
+    // This is a hot path so it should be really fast. The way it works
+    // is by converting the IP address to a number by removing non numeric
+    // characters, then compressing it to the number of slots we have.
+    //
+    // Compared against "real" hashing (from the sticky-session code) and
+    // "real" IP number conversion, this function is on par in terms of
+    // worker index distribution only much faster.
+    const worker_index = function (ip, len) {
+        return farmhash.fingerprint32(ip) % len; // Farmhash is the fastest and works with IPv6, too
+    };
+
+}
+
+
 const app = express();
 app.use(cors())
 const userRouter = require('./routers/userRoter')
